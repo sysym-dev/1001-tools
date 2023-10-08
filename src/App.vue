4c78c690e6cffbf2c53195e93ede0aa08c495f24
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-09 05:23:58 +0700

    feat: layout app

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,9 @@
 <script setup lang="ts">
-import {
-  NLayout,
-  NLayoutSider,
-  NLayoutContent,
-  NMenu,
-  MenuOption,
-  NIcon,
-} from 'naive-ui';
-import { h } from 'vue';
-import { Component } from 'vue';
-import {
-  MailInbox16Regular,
-  Archive16Regular,
-  Folder16Regular,
-} from '@vicons/fluent';
-import { ref } from 'vue';
-
-const menuOptions: MenuOption[] = [
-  {
-    label: 'Today Todo',
-    key: 'today',
-    icon: renderIcon(MailInbox16Regular),
-  },
-  {
-    label: 'Late Todo',
-    key: 'late',
-    icon: renderIcon(Archive16Regular),
-  },
-  {
-    label: 'All Todo',
-    key: 'all',
-    icon: renderIcon(Folder16Regular),
-  },
-];
-const isCollapsed = ref(false);
-
-function renderIcon(icon: Component) {
-  return () => h(NIcon, null, { default: () => h(icon) });
-}
+import LayoutApp from './components/layouts/layout-app.vue';
 </script>
 
 <template>
-  <n-layout has-sider content-style="min-height: 100vh">
-    <n-layout-sider
-      :bordered="true"
-      collapse-mode="width"
-      show-trigger
-      v-model="isCollapsed"
-    >
-      <n-menu :options="menuOptions" :collapsed="isCollapsed"></n-menu>
-    </n-layout-sider>
-    <n-layout content-style="padding: 24px" :embedded="true">
-      <n-layout-content content-style="padding: 24px">content</n-layout-content>
-    </n-layout>
-  </n-layout>
+  <layout-app>
+    <router-view />
+  </layout-app>
 </template>
---
+++ src/modules/todo/views/todo-today-page.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>oke</div>
+</template>
