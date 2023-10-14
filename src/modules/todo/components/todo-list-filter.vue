12622d0852f031bd1d69b23405f7a0929fc32ee7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-14 19:04:58 +0700

    feat: sort

--- src/modules/todo/components/todo-list-filter.vue
+++ src/modules/todo/components/todo-list-filter.vue
@@ -1,11 +1,72 @@
 <script setup lang="ts">
-import { NSpace, NButton, NIcon, NInput, NDropdown } from 'naive-ui';
+import {
+  NSpace,
+  NButton,
+  NIcon,
+  NInput,
+  NDropdown,
+  DropdownRenderOption,
+  NSelect,
+  SelectOption,
+  NFormItem,
+  NInputGroup,
+} from 'naive-ui';
 import { Add12Regular } from '@vicons/fluent';
 import { MoreHorizontal16Regular } from '@vicons/fluent';
 import TodoCreateModal from './todo-create-modal.vue';
-import { ref } from 'vue';
+import { ref, h } from 'vue';
 
 const visibleCreate = ref(false);
+const sortOptions: SelectOption[] = [
+  {
+    label: 'Name',
+    value: 'name',
+  },
+  {
+    label: 'Created At',
+    value: 'createdAt',
+  },
+  {
+    label: 'Done At',
+    value: 'done at',
+  },
+];
+const sortDirectionOptions: SelectOption[] = [
+  {
+    label: 'Asc',
+    value: 'asc',
+  },
+  {
+    label: 'Desc',
+    value: 'desc',
+  },
+];
+
+const filterOptions: DropdownRenderOption[] = [
+  {
+    key: 'sort',
+    label: 'Sort',
+    type: 'render',
+    render: () =>
+      h(
+        NFormItem,
+        { label: 'Sort', showFeedback: false },
+        {
+          default: () =>
+            h(NInputGroup, null, {
+              default: () => [
+                h(NSelect, { options: sortOptions, placeholder: 'Column' }),
+                h(NSelect, {
+                  style: { width: '60%' },
+                  options: sortDirectionOptions,
+                  placeholder: 'Asc',
+                }),
+              ],
+            }),
+        },
+      ),
+  },
+];
 
 function handleCreate() {
   visibleCreate.value = true;
@@ -15,7 +76,15 @@ function handleCreate() {
 <template>
   <n-space>
     <n-input placeholder="Search" />
-    <n-dropdown>
+    <n-dropdown
+      trigger="click"
+      placement="bottom-end"
+      :options="filterOptions"
+      :width="200"
+      :style="{
+        padding: '14px',
+      }"
+    >
       <n-button>
         <template #icon>
           <n-icon>
