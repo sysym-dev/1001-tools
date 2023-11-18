dadab889d5047f715f9fccd71715fdad9447d5c6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-11-18 13:27:29 +0700

    feat: redesign layout

--- src/components/layouts/layout-app.vue
+++ src/components/layouts/layout-app.vue
@@ -1,102 +1,127 @@
 <script setup lang="ts">
 import {
   NLayout,
-  NLayoutSider,
   NLayoutContent,
-  NMenu,
-  MenuOption,
+  NLayoutHeader,
   NIcon,
+  NSpace,
+  NButton,
+  NDropdown,
+  DropdownOption,
 } from 'naive-ui';
-import { computed, h } from 'vue';
 import { RouterLink } from 'vue-router';
-import { Component } from 'vue';
 import {
-  MailInbox16Regular,
-  Archive16Regular,
-  Folder16Regular,
-  Home16Regular,
+  PersonCircle24Regular,
+  ChevronDown16Regular,
+  ChevronUp16Regular,
 } from '@vicons/fluent';
 import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 
-const menuOptions: MenuOption[] = [
+const todoDropdown: DropdownOption[] = [
   {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: { name: 'dashboard' },
-        },
-        { default: () => 'Dashboard' },
-      ),
-    key: 'dashboard',
-    icon: renderIcon(Home16Regular),
-  },
-  {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: { name: 'today' },
-        },
-        { default: () => 'Today Todo' },
-      ),
     key: 'today',
-    icon: renderIcon(MailInbox16Regular),
+    label: 'Today',
   },
   {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: { name: 'late' },
-        },
-        { default: () => 'Late Todo' },
-      ),
     key: 'late',
-    icon: renderIcon(Archive16Regular),
+    label: 'Late',
   },
   {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: { name: 'all' },
-        },
-        { default: () => 'All Todo' },
-      ),
     key: 'all',
-    icon: renderIcon(Folder16Regular),
+    label: 'All Todo',
   },
 ];
-const isCollapsed = ref(false);
-const active = computed<string>(() => route.name as string);
 
-function renderIcon(icon: Component) {
-  return () => h(NIcon, null, { default: () => h(icon) });
+const visibleTodoDropdown = ref(false);
+
+function checkIsActive(key: string | string[]): boolean {
+  if (Array.isArray(key)) {
+    return key.includes(route.name as string);
+  }
+
+  return route.name === key;
+}
+
+function handleToggleTodoDropdown() {
+  visibleTodoDropdown.value = !visibleTodoDropdown.value;
+}
+function handleCloseTodoDropdown() {
+  visibleTodoDropdown.value = false;
+}
+function handleSelectTodoDropdown(key: string) {
+  router.push({ name: key });
 }
 </script>
 
 <template>
-  <n-layout has-sider content-style="min-height: 100vh">
-    <n-layout-sider
-      :bordered="true"
-      collapse-mode="width"
-      show-trigger
-      v-model="isCollapsed"
-    >
-      <n-menu
-        :options="menuOptions"
-        :collapsed="isCollapsed"
-        :value="active"
-      ></n-menu>
-    </n-layout-sider>
-    <n-layout
-      content-style="padding: 24px; background-color: rgb(250, 250, 252)"
-    >
-      <n-layout-content content-style="background-color: rgb(250, 250, 252)">
+  <n-layout
+    content-style="min-height: 100vh; background-color: rgb(250, 250, 252)"
+  >
+    <n-layout-header bordered>
+      <n-layout-content content-style="padding: 12px 24px">
+        <n-space justify="space-between">
+          <n-space>
+            <router-link
+              :to="{ name: 'dashboard' }"
+              custom
+              #="{ href, navigate }"
+            >
+              <n-button
+                :secondary="checkIsActive('dashboard')"
+                :quaternary="!checkIsActive('dashboard')"
+                :type="checkIsActive('dashboard') ? 'primary' : 'default'"
+                tag="a"
+                :href="href"
+                @click="navigate"
+              >
+                Dashboard
+              </n-button>
+            </router-link>
+            <n-dropdown
+              :options="todoDropdown"
+              :show="visibleTodoDropdown"
+              @clickoutside="handleCloseTodoDropdown"
+              @select="handleSelectTodoDropdown"
+            >
+              <n-button
+                :secondary="checkIsActive(['today', 'late', 'all'])"
+                :quaternary="!checkIsActive(['today', 'late', 'all'])"
+                :type="
+                  checkIsActive(['today', 'late', 'all'])
+                    ? 'primary'
+                    : 'default'
+                "
+                icon-placement="right"
+                @click="handleToggleTodoDropdown"
+              >
+                <template #icon>
+                  <n-icon>
+                    <ChevronUp16Regular v-if="visibleTodoDropdown" />
+                    <ChevronDown16Regular v-else />
+                  </n-icon>
+                </template>
+                <span>Todo</span>
+              </n-button>
+            </n-dropdown>
+          </n-space>
+          <n-button quaternary>
+            <template #icon>
+              <n-icon>
+                <PersonCircle24Regular />
+              </n-icon>
+            </template>
+          </n-button>
+        </n-space>
+      </n-layout-content>
+    </n-layout-header>
+    <n-layout>
+      <n-layout-content
+        content-style="padding: 24px; background-color: rgb(250, 250, 252)"
+      >
         <slot />
       </n-layout-content>
     </n-layout>
