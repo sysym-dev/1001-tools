f2e6386e13f16011a296c131256f9f021105651e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-11-25 14:42:16 +0700

    task list

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import TaskCategoryWidgetList from './modules/task-category/components/task-category-widget-list.vue';
+import TaskList from './modules/task/components/task-list.vue';
 </script>
 
 <template>
-  <div class="px-4 py-8 sm:py-12 container">
+  <div class="px-4 py-8 sm:py-12 container space-y-8">
     <task-category-widget-list />
+
+    <task-list />
   </div>
 </template>
---
+++ src/modules/task/components/task-list.vue
@@ -0,0 +1,67 @@
+<script setup>
+import BaseHeading from 'src/components/base/base-heading.vue';
+import BaseButton from 'src/components/base/base-button.vue';
+import { ChevronDownIcon } from '@heroicons/vue/24/outline';
+
+const tasks = [
+  {
+    id: 1,
+    name: 'Cek KHS UTS',
+    date: new Date(),
+  },
+  {
+    id: 2,
+    name: 'Tidur Setelar Asar',
+    date: new Date(),
+  },
+  {
+    id: 3,
+    name: 'Play DLS',
+    date: new Date(),
+  },
+  {
+    id: 4,
+    name: 'Nonton Siti',
+    date: new Date(),
+  },
+];
+</script>
+
+<template>
+  <div>
+    <base-heading title="Today Todo">
+      <template #actions>
+        <base-button color="sky">New</base-button>
+      </template>
+    </base-heading>
+
+    <ul class="divide-y divide-gray-100">
+      <li
+        v-for="task in tasks"
+        :key="task.id"
+        class="flex items-center justify-between gap-x-6 py-5"
+      >
+        <div class="min-w-0">
+          <p class="text-sm font-semibold leading-6 text-gray-900">
+            {{ task.name }}
+          </p>
+          <div
+            class="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500"
+          >
+            <p class="whitespace-nowrap">
+              Due on <time datetime="2023-03-17T00:00Z">today</time>
+            </p>
+          </div>
+        </div>
+        <base-button size="sm">
+          Todo
+          <template #append>
+            <ChevronDownIcon class="w-4 h-4" />
+          </template>
+        </base-button>
+      </li>
+    </ul>
+
+    <base-button fullwidth>See All</base-button>
+  </div>
+</template>
