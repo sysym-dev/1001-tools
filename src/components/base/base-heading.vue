7b3f768fb3437ab180fcea754fec36ba0f8ac924
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-11-29 06:58:38 +0700

    category detail page

--- src/components/base/base-heading.vue
+++ src/components/base/base-heading.vue
@@ -3,12 +3,22 @@ import BaseTitle from './base-title.vue';
 
 const props = defineProps({
   title: String,
+  withDescription: {
+    type: Boolean,
+    default: false,
+  },
+  description: String,
 });
 </script>
 
 <template>
   <div class="flex items-center justify-between">
-    <base-title>{{ title }}</base-title>
+    <div>
+      <base-title>{{ title }}</base-title>
+      <p v-if="withDescription" class="mt-2 text-sm text-gray-500">
+        {{ description }}
+      </p>
+    </div>
     <slot name="actions" />
   </div>
 </template>
---
+++ src/modules/task-category/views/task-category-index.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { PlusIcon } from '@heroicons/vue/24/outline';
+import BaseHeading from 'src/components/base/base-heading.vue';
+import BaseButton from 'src/components/base/base-button.vue';
+import TaskList from 'src/modules/task/components/task-list.vue';
+</script>
+
+<template>
+  <base-heading
+    title="Shopping List"
+    description="2 / 10 Completed"
+    with-description
+  >
+    <template #actions>
+      <base-button :classes="{ base: 'hidden sm:block' }" color="sky"
+        >New Task</base-button
+      >
+    </template>
+  </base-heading>
+  <task-list />
+  <base-button fullwidth>Load More</base-button>
+  <base-button
+    :classes="{ base: 'fixed bottom-4 right-4 sm:hidden' }"
+    color="sky"
+    size="xl"
+    circle
+  >
+    <plus-icon class="w-5 h-5" />
+  </base-button>
+</template>
