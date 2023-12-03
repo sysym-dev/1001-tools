c8965ee41150bcf2aeb20254f911e377970cbefe
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-03 16:16:49 +0700

    base input error state

--- src/components/base/base-input.vue
+++ src/components/base/base-input.vue
@@ -1,12 +1,26 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const props = defineProps({
   label: String,
   placeholder: String,
+  state: {
+    type: String,
+    default: 'normal',
+  },
+  message: String,
 });
 
 const inputEl = ref();
+
+const state = computed(() => {
+  return {
+    normal:
+      'text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-sky-600',
+    error:
+      'text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500',
+  }[props.state];
+});
 
 defineExpose({ inputEl });
 </script>
@@ -18,7 +32,11 @@ defineExpose({ inputEl });
   <input
     ref="inputEl"
     type="text"
-    class="block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 text-sm leading-6"
+    :class="[
+      'block w-full rounded-md border-0 py-1.5 ring-1 ring-inset focus:ring-2 focus:ring-inset text-sm leading-6',
+      state,
+    ]"
     :placeholder="placeholder"
   />
+  <p v-if="message" class="mt-2 text-sm text-red-600">{{ message }}</p>
 </template>
