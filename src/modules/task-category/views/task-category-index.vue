f1268f46ab682cbaf3eb2795b220723de37b5b90
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-03 07:35:27 +0700

    base tab

--- src/modules/task-category/views/task-category-index.vue
+++ src/modules/task-category/views/task-category-index.vue
@@ -2,29 +2,56 @@
 import { PlusIcon } from '@heroicons/vue/24/outline';
 import BaseHeading from 'src/components/base/base-heading.vue';
 import BaseButton from 'src/components/base/base-button.vue';
+import BaseTabs from 'src/components/base/base-tabs.vue';
 import TaskList from 'src/modules/task/components/task-list.vue';
+import { h, ref } from 'vue';
+
+const tabs = [
+  {
+    id: 'tasks',
+    name: 'Task',
+    component: () =>
+      h('div', {}, [
+        h(TaskList),
+        h(
+          BaseButton,
+          { fullwidth: true },
+          {
+            default: () => 'Load More',
+          },
+        ),
+      ]),
+  },
+  {
+    id: 'setting',
+    name: 'Setting',
+  },
+];
+
+const activeTab = ref('tasks');
 </script>
 
 <template>
-  <base-heading
-    title="Shopping List"
-    description="2 / 10 Completed"
-    with-description
-  >
-    <template #actions>
-      <base-button :classes="{ base: 'hidden sm:block' }" color="sky"
-        >New Task</base-button
-      >
-    </template>
-  </base-heading>
-  <task-list />
-  <base-button fullwidth>Load More</base-button>
-  <base-button
-    :classes="{ base: 'fixed bottom-4 right-4 sm:hidden' }"
-    color="sky"
-    size="xl"
-    circle
-  >
-    <plus-icon class="w-5 h-5" />
-  </base-button>
+  <div class="space-y-2">
+    <base-heading
+      title="Shopping List"
+      description="2 / 10 Completed"
+      with-description
+    >
+      <template #actions>
+        <base-button :classes="{ base: 'hidden sm:block' }" color="sky"
+          >New Task</base-button
+        >
+        <base-button
+          :classes="{ base: 'fixed bottom-4 right-4 z-10 sm:hidden' }"
+          color="sky"
+          size="xl"
+          circle
+        >
+          <plus-icon class="w-5 h-5" />
+        </base-button>
+      </template>
+    </base-heading>
+    <base-tabs :tabs="tabs" v-model:active="activeTab" />
+  </div>
 </template>
