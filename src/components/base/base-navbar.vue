67ee287b3c23f29b15c5a23722ed71bc38f3fd64
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-09 13:55:14 +0700

    base navbar

---
+++ src/components/base/base-navbar.vue
@@ -0,0 +1,74 @@
+<script setup>
+import BaseButton from './base-button.vue';
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline';
+import { ref } from 'vue';
+
+const props = defineProps({
+  menus: {
+    type: Array,
+    required: true,
+  },
+  active: String,
+});
+
+const visibleMobileMenu = ref(false);
+
+function checkIsActive(menu) {
+  return menu.id === props.active;
+}
+
+function handleToggleMobileMenu() {
+  visibleMobileMenu.value = !visibleMobileMenu.value;
+}
+</script>
+
+<template>
+  <div class="border-b">
+    <div class="container px-4 h-16 flex items-center">
+      <div class="hidden space-x-8 h-full sm:flex">
+        <router-link
+          v-for="menu in menus"
+          :key="menu.id"
+          :to="menu.to"
+          :class="[
+            'h-full flex items-center px-1 text-sm font-medium border-b-2',
+            checkIsActive(menu)
+              ? 'text-gray-900 border-b-2 border-sky-500'
+              : 'text-gray-500 border-transparent hover:border-gray-300',
+          ]"
+        >
+          {{ menu.name }}
+        </router-link>
+      </div>
+      <div class="sm:hidden">
+        <base-button
+          custom-color="text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500"
+          custom-size="p-2 rounded-md"
+          v-on:click="handleToggleMobileMenu"
+        >
+          <component
+            :is="visibleMobileMenu ? XMarkIcon : Bars3Icon"
+            class="block h-6 w-6"
+          />
+        </base-button>
+      </div>
+    </div>
+    <div v-if="visibleMobileMenu" class="sm:hidden">
+      <div class="space-y-1 pb-2">
+        <a
+          v-for="menu in menus"
+          :key="menu.id"
+          href="#"
+          :class="[
+            'block border-l-4 py-2 pl-3 pr-4 text-base font-medium',
+            checkIsActive(menu)
+              ? 'border-sky-500 bg-sky-50 text-sky-700'
+              : 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700',
+          ]"
+        >
+          {{ menu.name }}
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
