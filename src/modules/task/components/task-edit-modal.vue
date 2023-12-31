f1974e0eb084d60b1d929d7465fec36ba966c1dd
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-31 08:39:11 +0700

    feat: edited event after task edited

--- src/modules/task/components/task-edit-modal.vue
+++ src/modules/task/components/task-edit-modal.vue
@@ -4,7 +4,7 @@ import BaseCard from 'src/components/base/base-card.vue';
 import BaseInput from 'src/components/base/base-input.vue';
 import BaseButton from 'src/components/base/base-button.vue';
 import WithState from 'src/components/composes/with-state.vue';
-import { computed, nextTick, ref } from 'vue';
+import { computed, inject, nextTick, ref } from 'vue';
 import { object, string } from 'yup';
 import { useForm } from 'src/composes/form.compose';
 import { useRequest } from 'src/composes/request.compose';
@@ -20,6 +20,7 @@ const props = defineProps({
   },
 });
 const emit = defineEmits(['update:visible', 'close']);
+const emitter = inject('emitter');
 
 const {
   isLoading,
@@ -78,6 +79,10 @@ async function handleSubmit() {
       url: `/tasks/${props.task.id}`,
       data: form.value,
     });
+
+    visible.value = false;
+
+    emitter.emit('tasks-edited');
   } catch (err) {
     //
   }
