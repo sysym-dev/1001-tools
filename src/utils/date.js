2fbd14ef77b6743a1728f57d8161ff03de39fba4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-02 06:04:59 +0700

    feat: date object support

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import objectSupport from 'dayjs/plugin/objectSupport';
 
+dayjs.extend(objectSupport);
 dayjs.extend(relativeTime);
 
 export function date(string) {
