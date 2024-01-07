900cd94a528337144f2cdd1e2b52d726b78e48c1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-07 14:05:41 +0700

    feat: register

--- src/modules/auth/auth.store.js
+++ src/modules/auth/auth.store.js
@@ -3,6 +3,15 @@ import { ref } from 'vue';
 
 export const useAuthStore = defineStore('auth', () => {
   const isLoggedIn = ref(false);
+  const accessToken = ref(null);
+  const me = ref(null);
 
-  return { isLoggedIn };
+  function login(payload) {
+    accessToken.value = payload.accessToken;
+    me.value = payload.me;
+
+    isLoggedIn.value = true;
+  }
+
+  return { isLoggedIn, login };
 });
