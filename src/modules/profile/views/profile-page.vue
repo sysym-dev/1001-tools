a4e77c4fa2c8c595e7cd103496364b19fc3e2285
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-09 18:52:41 +0700

    feat: edit profile

--- src/modules/profile/views/profile-page.vue
+++ src/modules/profile/views/profile-page.vue
@@ -2,9 +2,13 @@
 import BaseHeading from 'src/components/base/base-heading.vue';
 import BaseDescriptionColumn from 'src/components/base/base-description-column.vue';
 import BaseButton from 'src/components/base/base-button.vue';
+import ProfileEditModal from 'src/modules/profile/components/profile-edit-modal.vue';
 import { useAuthStore } from 'src/modules/auth/auth.store';
+import { ref } from 'vue';
 
 const authStore = useAuthStore();
+
+const visibleEditModal = ref(false);
 
 const columns = [
   {
@@ -16,13 +20,20 @@ const columns = [
     name: 'Email',
   },
 ];
+
+function handleEdit() {
+  visibleEditModal.value = true;
+}
 </script>
 
 <template>
   <base-heading title="Profile">
     <template #actions>
-      <base-button color="sky">Edit Profile</base-button>
+      <base-button color="sky" v-on:click="handleEdit"
+        >Edit Profile</base-button
+      >
     </template>
   </base-heading>
   <base-description-column :columns="columns" :data="authStore.me" />
+  <profile-edit-modal v-model:visible="visibleEditModal" />
 </template>
