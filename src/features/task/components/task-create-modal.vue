62a1afee3a2b7f40f8126394470f9cd7bf924598
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-29 18:42:58 +0700

    fix: task due at incorrect values

--- src/features/task/components/task-create-modal.vue
+++ src/features/task/components/task-create-modal.vue
@@ -94,7 +94,7 @@ async function handleSubmit() {
       data: {
         ...form.value,
         due_at: form.value.due_at
-          ? createDate(form.value.due_at).startOf('d')
+          ? createDate(form.value.due_at).endOf('d')
           : null,
       },
     });
