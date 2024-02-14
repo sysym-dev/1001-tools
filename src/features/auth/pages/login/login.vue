eecfb188ab4e0cbed002de825221e92fa352ca8b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-14 12:01:18 +0700

    feat: login form element

---
+++ src/features/auth/pages/login/login.vue
@@ -0,0 +1,7 @@
+<template>
+  <form id="login">
+    <input type="email" name="email" id="email" />
+    <input type="password" name="password" id="password" />
+    <button name="submit" id="submit">Login</button>
+  </form>
+</template>
