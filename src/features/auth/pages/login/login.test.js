08b06dfd55f59e0770324a320503877793141e7f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-14 20:24:51 +0700

    feat: login element and validation

--- src/features/auth/pages/login/login.test.js
+++ src/features/auth/pages/login/login.test.js
@@ -1,32 +1,129 @@
-import { mount } from '@vue/test-utils';
-import { describe, expect, test } from 'vitest';
+import { flushPromises, mount } from '@vue/test-utils';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 import Login from './login.vue';
+import BaseButton from 'src/components/base/base-button.vue';
+import { validateSchema } from 'src/core/validation/validate-schema';
+import { ValidationError } from 'src/core/validation/validation-error';
+import { nextTick } from 'vue';
+
+vi.mock('src/core/validation/validate-schema');
+vi.mock('src/core/validation/validation-error');
 
 describe('login.vue', () => {
-  test('render form', () => {
-    const wrapper = mount(Login);
+  let wrapper;
+  let formLogin;
 
-    expect(wrapper.find('form#login').exists()).toBe(true);
+  function findInput(id) {
+    return formLogin.findComponent(`#${id}`);
+  }
+  function findSubmitBtn() {
+    return formLogin.findComponent(BaseButton);
+  }
+  async function triggerSubmitForm() {
+    await formLogin.trigger('submit');
+  }
+  async function triggerErrorSubmitForm(options = {}) {
+    ValidationError.mockImplementation(function () {
+      this.details = options.details ?? {
+        email: 'email is required',
+        password: 'password is required',
+      };
+
+      return this;
+    });
+
+    const error = new ValidationError();
+
+    validateSchema.mockRejectedValue(error);
+
+    await triggerSubmitForm();
+
+    return { error };
+  }
+
+  beforeEach(() => {
+    validateSchema.mockReset();
+
+    wrapper = mount(Login);
+    formLogin = wrapper.find('form#login');
   });
 
-  test('render input email', () => {
-    const wrapper = mount(Login);
+  test('render form element', () => {
+    expect(formLogin.exists()).toBe(true);
 
-    expect(wrapper.find('input#email[name=email]').exists()).toBe(true);
-  });
+    const inputEmail = findInput('email');
+    const inputPassword = findInput('password');
+    const submitBtn = findSubmitBtn();
 
-  test('render input password', () => {
-    const wrapper = mount(Login);
+    expect(inputEmail.exists()).toBe(true);
+    expect(inputEmail.props('type')).toBe('email');
+    expect(inputEmail.props('hasMessage')).toBe(false);
 
-    expect(wrapper.find('input#password[name=password]').exists()).toBe(true);
-  });
-
-  test('render login button', () => {
-    const wrapper = mount(Login);
-
-    const submitBtn = wrapper.find('button#submit[name=submit]');
+    expect(inputPassword.exists()).toBe(true);
+    expect(inputPassword.props('type')).toBe('password');
+    expect(inputPassword.props('hasMessage')).toBe(false);
 
     expect(submitBtn.exists()).toBe(true);
+    expect(submitBtn.props('type')).toBe('submit');
     expect(submitBtn.text()).toBe('Login');
+    expect(submitBtn.props('isLoading')).toBe(false);
+  });
+
+  test('toggle button loading on form submit and after submit', async () => {
+    triggerSubmitForm();
+
+    const submitBtn = findSubmitBtn();
+
+    await nextTick();
+
+    expect(submitBtn.props('isLoading')).toBe(true);
+
+    await flushPromises();
+
+    expect(submitBtn.props('isLoading')).toBe(false);
+  });
+
+  test('shows input element error messages when validate form error', async () => {
+    const { error } = await triggerErrorSubmitForm();
+
+    expect(validateSchema).toHaveBeenCalledWith({
+      email: '',
+      password: '',
+    });
+
+    const inputEmail = findInput('email');
+    const inputPassword = findInput('password');
+
+    expect(inputEmail.props('hasMessage')).toBe(true);
+    expect(inputEmail.props('message')).toBe(error.details.email);
+    expect(inputPassword.props('hasMessage')).toBe(true);
+    expect(inputPassword.props('message')).toBe(error.details.password);
+  });
+
+  test('reset input element error messages when submit', async () => {
+    await triggerErrorSubmitForm();
+
+    const email = 'test@email.com';
+
+    await findInput('email').find('input').setValue(email);
+
+    const { error } = await triggerErrorSubmitForm({
+      details: {
+        password: 'password is required',
+      },
+    });
+
+    expect(validateSchema).toHaveBeenCalledWith({
+      email,
+      password: '',
+    });
+
+    const inputEmail = findInput('email');
+    const inputPassword = findInput('password');
+
+    expect(inputEmail.props('hasMessage')).toBe(false);
+    expect(inputEmail.props('message')).toBeUndefined();
+    expect(inputPassword.props('hasMessage')).toBe(true);
+    expect(inputPassword.props('message')).toBe(error.details.password);
   });
 });
--- src/features/auth/pages/login/login.vue
+++ src/features/auth/pages/login/login.vue
@@ -1,7 +1,51 @@
+<script setup>
+import BaseInput from 'src/components/base/base-input.vue';
+import BaseButton from 'src/components/base/base-button.vue';
+import { reactive, ref } from 'vue';
+import { validateSchema } from 'src/core/validation/validate-schema';
+import { ValidationError } from 'src/core/validation/validation-error';
+
+const form = reactive({
+  email: '',
+  password: '',
+});
+const isLoading = ref(false);
+const errors = ref({
+  email: null,
+  password: null,
+});
+
+async function handleSubmit() {
+  isLoading.value = true;
+
+  try {
+    await validateSchema(form);
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      errors.value = err.details;
+    }
+  } finally {
+    isLoading.value = false;
+  }
+}
+</script>
+
 <template>
-  <form id="login">
-    <input type="email" name="email" id="email" />
-    <input type="password" name="password" id="password" />
-    <button name="submit" id="submit">Login</button>
+  <form id="login" v-on:submit="handleSubmit">
+    <base-input
+      id="email"
+      type="email"
+      :has-message="!!errors.email"
+      :message="errors.email"
+      v-model="form.email"
+    />
+    <base-input
+      id="password"
+      type="password"
+      :has-message="!!errors.password"
+      :message="errors.password"
+      v-model="form.password"
+    />
+    <base-button type="submit" :is-loading="isLoading">Login</base-button>
   </form>
 </template>
