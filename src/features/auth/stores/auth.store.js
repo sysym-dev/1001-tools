eea39c077343a693765216aca2d03e0f250ea948
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-18 03:39:35 +0700

    feat: auth store

--- src/features/auth/stores/auth.store.js
+++ src/features/auth/stores/auth.store.js
@@ -1,1 +1,24 @@
-export function useAuthStore() {}
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useAuthStore = defineStore('auth', () => {
+  const isLoggedIn = ref(false);
+  const accessToken = ref(null);
+  const me = ref({
+    id: null,
+  });
+
+  function login(payload) {
+    accessToken.value = payload.accessToken;
+    me.value = payload.me;
+
+    isLoggedIn.value = true;
+  }
+
+  return {
+    isLoggedIn,
+    accessToken,
+    me,
+    login,
+  };
+});
