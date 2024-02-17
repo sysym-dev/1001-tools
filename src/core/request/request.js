cf13386061b8b6a8dbab88c4916f849e7da60f02
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-17 07:37:11 +0700

    feat: request

--- src/core/request/request.js
+++ src/core/request/request.js
@@ -1,1 +1,16 @@
-export function request() {}
+import axios from 'axios';
+import { RequestError } from './request.error';
+
+export async function request(url, options = {}) {
+  try {
+    const res = await axios({
+      url,
+      method: options.method ?? 'get',
+      ...options,
+    });
+
+    return res.data;
+  } catch (err) {
+    throw new RequestError(err.response.data.message);
+  }
+}
