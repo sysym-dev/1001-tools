e24a0f56afcd08e3b1772a640987e58e1f962154
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-17 14:10:00 +0700

    feat: validation error

--- src/core/validation/validation.error.js
+++ src/core/validation/validation.error.js
@@ -1,1 +1,17 @@
-export class ValidationError {}
+import yup from 'yup';
+
+export class ValidationError {
+  details;
+
+  constructor(error) {
+    if (error instanceof yup.ValidationError) {
+      this.details = Object.fromEntries(
+        error.errors.map((item) => {
+          return [item.path, item.errors];
+        }),
+      );
+    } else {
+      this.details = {};
+    }
+  }
+}
