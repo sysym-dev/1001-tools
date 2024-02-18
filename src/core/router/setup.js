e1ce79d39b608986bfd7104d38c58347d291e41e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-18 07:55:50 +0700

    feat: setup routes file

--- src/core/router/setup.js
+++ src/core/router/setup.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from 'src/routes/index';
 
 export function setupRouter(app) {
   app.use(
     createRouter({
       history: createWebHistory(),
-      routes: [],
+      routes,
     }),
   );
 }
