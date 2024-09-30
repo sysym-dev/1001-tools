18966983b6c2419b4889790485bfcb49ddb4cdd1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-30 10:45:50 +0700

    index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,26 +1,34 @@
 <script setup>
-import BaseCard from 'src/components/base-card.vue';
 import { ArrowsLeftRight } from '@vicons/tabler';
 
 const menus = [
-  { id: 'json-to-toml', name: 'JSON to TOML', icon: ArrowsLeftRight },
+  {
+    id: 'json',
+    name: 'JSON',
+    children: [
+      { id: 'json-to-toml', name: 'JSON to TOML', icon: ArrowsLeftRight },
+    ],
+  },
 ];
 </script>
 
 <template>
-  <base-card>
-    <nav>
-      <ul class="space-y-1">
-        <li v-for="menu in menus" :key="menu.id">
-          <router-link
-            :to="menu.id"
-            class="hover:underline flex items-center gap-x-2"
-          >
-            <component :is="menu.icon" class="w-4 h-4" />
-            {{ menu.name }}
-          </router-link>
-        </li>
-      </ul>
-    </nav>
-  </base-card>
+  <nav>
+    <div v-for="menu in menus" :key="menu.id">
+      <div class="space-y-2 divide-y">
+        <p class="text-lg font-medium">{{ menu.name }}</p>
+        <ul class="space-y-1 pt-2">
+          <li v-for="childMenu in menu.children" :key="childMenu.id">
+            <router-link
+              :to="childMenu.id"
+              class="hover:underline flex items-center gap-x-2"
+            >
+              <component :is="childMenu.icon" class="w-4 h-4" />
+              {{ childMenu.name }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
