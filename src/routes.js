0cd924cbcea40a73c18f48683034613fed5bd385
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-10-02 07:25:40 +0700

    meta per route

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,9 @@ export const routes = [
     path: '/',
     name: 'index',
     meta: {
-      title: import.meta.env.VITE_APP_TITLE,
-      description: import.meta.env.VITE_APP_DESCRIPTION,
+      title: 'Free Online Tools for Everyday Tasks',
+      description:
+        'Discover 1001 Tools, offering a wide range of free online tools to simplify your daily tasks. Find calculators, converters, measurement tools, and more, all in one place',
       keywords: ['tools', 'web tools', 'developer tools'],
       withHeader: false,
     },
@@ -15,7 +16,8 @@ export const routes = [
     name: 'json-to-toml',
     meta: {
       title: 'JSON to TOML',
-      description: 'Convert from JSON to TOML',
+      description:
+        'Easily convert JSON to TOML with our free online converter. Fast, accurate, and simple to use, perfect for developers and data format conversions in seconds.',
       keywords: ['json to toml', 'json', 'toml', 'convert'],
     },
     component: () => import('./tools/json-to-toml/index.vue'),
