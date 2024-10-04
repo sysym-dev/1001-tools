bbfad441d07d9e7efb2c543ddc659b4d2a9cc5e4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-10-04 20:02:14 +0700

    partial progress bar

---
+++ src/components/partials/partial-progress-bar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { getCurrentInstance, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const { $Progress } = getCurrentInstance().appContext.config.globalProperties;
+const router = useRouter();
+
+$Progress.start();
+
+router.beforeEach(() => {
+  $Progress.start();
+});
+
+router.afterEach(() => {
+  $Progress.finish();
+});
+
+onMounted(() => {
+  $Progress.finish();
+});
+</script>
+
+<template>
+  <vue-progress-bar />
+</template>
