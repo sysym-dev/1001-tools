d9cb6488164c04d217a4a052e57533d3ab4710dd
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-10-23 10:44:47 +0700

    base alert

---
+++ src/components/base/base-alert.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { AlertTriangle as AlertIcon, X as CloseIcon } from '@vicons/tabler';
+
+const emit = defineEmits(['close']);
+
+function onClose() {
+  emit('close');
+}
+</script>
+
+<template>
+  <div
+    class="bg-red-50 text-red-800 rounded-lg px-3 py-2 text-left font-semibold flex items-start justify-between"
+  >
+    <div class="flex gap-x-3">
+      <alert-icon class="w-4 h-4 mt-1" />
+      <slot />
+    </div>
+    <button class="mt-1" @click="onClose">
+      <close-icon class="w-4 h-4" />
+    </button>
+  </div>
+</template>
