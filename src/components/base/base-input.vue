1485bcb9b2ee8c9ad7be9ef879133fa23ade2722
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-11-06 20:27:49 +0700

    base input

---
+++ src/components/base/base-input.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  box: Boolean,
+});
+</script>
+
+<template>
+  <div
+    v-if="box"
+    class="rounded-lg px-3 py-2 pb-3 border border-gray-300 min-h-[200px] text-gray-900"
+  >
+    <slot />
+  </div>
+</template>
